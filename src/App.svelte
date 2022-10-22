a6b39011f10e1af8ff617a4b583ee38021ffbb7c
Fatih <70134562+fatihguvenc@users.noreply.github.com>
2022-10-22 14:04:27 +0300

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Search from "./components/Search.svelte";
+
+  let search = "";
+</script>
+
+<input type="text" bind:value={search} />
+<div class="center">
+  {#each search as item}
+    <Search {item} />
+  {/each}
+</div>
---
+++ src/components/Search.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let item;
+</script>
+
+<div class="name">
+  {item.toUpperCase()}
+  <img src={`https://sozluk.gov.tr/assets/img/isaret/${item}.gif`} alt="" />
+</div>
